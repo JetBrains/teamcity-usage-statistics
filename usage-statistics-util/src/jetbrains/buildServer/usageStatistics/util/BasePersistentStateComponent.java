5c3ab5408c5cf3cd08bedd19d46b6da8bc68f825
Maxim.Manuylov <Maxim.Manuylov@jetbrains.com>
2014-11-24 17:43:41 +0000

    extract general utility to persist plugin state

--- usage-statistics-util/src/jetbrains/buildServer/usageStatistics/util/BasePersistentStateComponent.java
+++ usage-statistics-util/src/jetbrains/buildServer/usageStatistics/util/BasePersistentStateComponent.java
@@ -16,63 +16,28 @@
 
 package jetbrains.buildServer.usageStatistics.util;
 
-import java.io.File;
-import java.io.IOException;
-import jetbrains.buildServer.serverSide.BuildServerAdapter;
 import jetbrains.buildServer.serverSide.SBuildServer;
 import jetbrains.buildServer.serverSide.ServerPaths;
-import jetbrains.buildServer.util.ExceptionUtil;
-import jetbrains.buildServer.util.FileUtil;
-import org.jdom.Element;
+import jetbrains.buildServer.util.BasePluginStatePersister;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class BasePersistentStateComponent extends BuildServerAdapter {
-  @NotNull private final File myFile;
-
+public abstract class BasePersistentStateComponent extends BasePluginStatePersister {
   public BasePersistentStateComponent(@NotNull final SBuildServer server, @NotNull final ServerPaths serverPaths) {
-    myFile = new File(getDataDir(serverPaths), getId() + ".xml");
-    server.addListener(this);
+    super(server, serverPaths);
   }
 
+  @NotNull
   @Override
-  public void serverStartup() {
-    loadState();
+  protected String getPluginName() {
+    return "usage-statistics";
   }
 
+  @NotNull
   @Override
-  public void serverShutdown() {
-    saveState();
+  protected String getStateName() {
+    return getId();
   }
 
   @NotNull
   protected abstract String getId();
-
-  protected abstract void writeExternal(@NotNull Element element);
-
-  protected abstract void readExternal(@NotNull Element element);
-
-  @NotNull
-  private File getDataDir(@NotNull final ServerPaths serverPaths) {
-    try {
-      return FileUtil.createDir(new File(serverPaths.getPluginDataDirectory(), "usage-statistics"));
-    }
-    catch (final IOException e) {
-      ExceptionUtil.rethrowAsRuntimeException(e);
-      //noinspection ConstantConditions
-      return null;
-    }
-  }
-
-  private void saveState() {
-    final Element root = new Element("root");
-    writeExternal(root);
-    XmlUtil.saveXml(root, myFile);
-  }
-
-  private void loadState() {
-    final Element root = XmlUtil.loadXml(myFile);
-    if (root != null) {
-      readExternal(root);
-    }
-  }
 }
