11af7f87c730712160bd32b3eb6de3bd193ea01f
Dmitry.Neverov <Dmitry.Neverov@jetbrains.com>
2015-08-14 12:04:26 +0000

    TW-41854 usage statistics for number of projects where versioned settings are configured

---
+++ usage-statistics-impl/src/jetbrains/buildServer/usageStatistics/impl/providers/VersionedSettingsUsageStatisticsProvider.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.usageStatistics.impl.providers;
+
+import jetbrains.buildServer.serverSide.ProjectManager;
+import jetbrains.buildServer.serverSide.SProject;
+import jetbrains.buildServer.serverSide.versionedSettings.VersionedSettingsManager;
+import jetbrains.buildServer.usageStatistics.presentation.UsageStatisticsGroupPosition;
+import jetbrains.buildServer.util.positioning.PositionAware;
+import jetbrains.buildServer.vcs.SVcsRoot;
+import org.jetbrains.annotations.NotNull;
+
+public class VersionedSettingsUsageStatisticsProvider extends BaseExtensionUsageStatisticsProvider {
+
+  private final ProjectManager myProjectManager;
+  private final VersionedSettingsManager myVersionedSettingsManager;
+
+  public VersionedSettingsUsageStatisticsProvider(@NotNull ProjectManager projectManager,
+                                                  @NotNull VersionedSettingsManager versionedSettingsManager) {
+    myProjectManager = projectManager;
+    myVersionedSettingsManager = versionedSettingsManager;
+  }
+
+  @Override
+  protected void collectUsages(@NotNull UsagesCollectorCallback callback) {
+    for (SProject p : myProjectManager.getProjects()) {
+      SVcsRoot root = myVersionedSettingsManager.getOwnVersionedSettingsVcsRoot(p);
+      if (root != null)
+        callback.addUsage(root.getVcsName(), root.getVcsDisplayName());
+    }
+  }
+
+  @NotNull
+  @Override
+  protected String getValueTooltip() {
+    return "Projects count (% of projects with versioned settings enabled)";
+  }
+
+  @NotNull
+  @Override
+  protected PositionAware getGroupPosition() {
+    return UsageStatisticsGroupPosition.VCS_FEATURES;
+  }
+}
