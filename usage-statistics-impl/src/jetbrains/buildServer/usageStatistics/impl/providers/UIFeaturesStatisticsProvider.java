57b7880bcecce1f177a275a86149281868dddda8
Oleg Rybak <oleg.rybak@jetbrains.com>
2019-05-17 16:56:17 +0300

    TW-59154 Gather statistics only for active web users. Add periods (day/week/month) to statistics

--- usage-statistics-impl/src/jetbrains/buildServer/usageStatistics/impl/providers/UIFeaturesStatisticsProvider.java
+++ usage-statistics-impl/src/jetbrains/buildServer/usageStatistics/impl/providers/UIFeaturesStatisticsProvider.java
@@ -17,16 +17,20 @@
 package jetbrains.buildServer.usageStatistics.impl.providers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import jetbrains.buildServer.usageStatistics.UsageStatisticsPublisher;
+import jetbrains.buildServer.usageStatistics.presentation.UsageStatisticsFormatter;
 import jetbrains.buildServer.usageStatistics.presentation.UsageStatisticsGroupPosition;
 import jetbrains.buildServer.usageStatistics.presentation.UsageStatisticsPresentationManager;
+import jetbrains.buildServer.usageStatistics.presentation.formatters.PercentageFormatter;
 import jetbrains.buildServer.users.SimplePropertyKey;
-import jetbrains.buildServer.users.UserModel;
+import jetbrains.buildServer.users.UserModelEx;
+import jetbrains.buildServer.util.CollectionsUtil;
 import jetbrains.buildServer.util.positioning.PositionAware;
 import org.jetbrains.annotations.NotNull;
 
-public class UIFeaturesStatisticsProvider extends BaseDefaultUsageStatisticsProvider {
+public class UIFeaturesStatisticsProvider extends BaseDynamicUsageStatisticsProvider {
 
   // map of user properties names to display names
   private static final Map<String, String> PROPERTIES = new HashMap<String, String>() {{
@@ -35,28 +39,40 @@ public class UIFeaturesStatisticsProvider extends BaseDefaultUsageStatisticsProv
   }};
 
   @NotNull
-  private final UserModel myUserModel;
+  private final UserModelEx myUserModel;
 
-  public UIFeaturesStatisticsProvider(@NotNull final UserModel userModel) {
+  @NotNull
+  private final WebUsersProvider myWebUsersProvider;
+
+  public UIFeaturesStatisticsProvider(@NotNull final UserModelEx userModel,
+                                      @NotNull final WebUsersProvider webUsersProvider) {
+    super(createDWMPeriodDescriptions(), new PercentageFormatter(1).format(0));
     myUserModel = userModel;
+    myWebUsersProvider = webUsersProvider;
   }
 
   @Override
   protected void accept(@NotNull final UsageStatisticsPublisher publisher,
-                        @NotNull final UsageStatisticsPresentationManager presentationManager) {
+                        @NotNull final UsageStatisticsPresentationManager presentationManager,
+                        @NotNull final String periodDescription,
+                        final long startDate) {
 
     final Map<SimplePropertyKey, Integer> counters = new HashMap<>();
     PROPERTIES.keySet().forEach(key -> counters.put(new SimplePropertyKey(key), 0));
+    final List<Long> userIds = CollectionsUtil.convertCollection(myWebUsersProvider.getWebUsers(startDate), Long::valueOf);
 
-    myUserModel.getAllUsers().getUsers().forEach(user ->
-                                                   counters.forEach((key, value) -> {
-                                                     if (user.getBooleanProperty(key)) {
-                                                       counters.put(key, counters.get(key) + 1);
-                                                     }
-                                                   }));
+    final String valueTooltip = "User count (% of active web users)";
+    final UsageStatisticsFormatter formatter = new PercentageFormatter(userIds.size());
+
+    myUserModel.findUsersByIds(userIds).forEach(user ->
+                                                  counters.forEach((key, value) -> {
+                                                    if (user.getBooleanProperty(key)) {
+                                                      counters.put(key, counters.get(key) + 1);
+                                                    }
+                                                  }));
     counters.forEach((simplePropertyKey, count) -> {
-      final String id = makeId("feature." + simplePropertyKey.getKey());
-      presentationManager.applyPresentation(id, PROPERTIES.get(simplePropertyKey.getKey()), myGroupName, null, null);
+      final String id = makeId("feature", periodDescription, simplePropertyKey.getKey());
+      presentationManager.applyPresentation(id, PROPERTIES.get(simplePropertyKey.getKey()), myGroupName, formatter, valueTooltip);
       publisher.publishStatistic(id, count);
     });
   }
@@ -66,4 +82,10 @@ public class UIFeaturesStatisticsProvider extends BaseDefaultUsageStatisticsProv
   protected PositionAware getGroupPosition() {
     return UsageStatisticsGroupPosition.UI_FEATURES;
   }
+
+  @Override
+  protected boolean mustSortStatistics() {
+    return false;
+  }
 }
+
